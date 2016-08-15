7e55032b42e48b8ca5ca5dd33751b3b61da9f91f
2123459746 <chia.chang@ge.com>
2016-08-15 01:03:51 -0700

    Adding variables.

--- build.sh
+++ build.sh
@@ -134,6 +134,7 @@ eval "sed -i -e 's#{SQLDSN}#${SQLDSN}#g' ./Dockerfile"
 
 eval "sed -i -e 's#{HOST}#pcs-backend-${ENV}.${HOST}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{BASE}#/${REV}/api#g' ./assets/swagger.json"
+eval "sed -i -e 's#{ENV}#/${ENV}#g' ./assets/swagger.json"
 
 docker_run
 cf_push
