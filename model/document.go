bcd8041b424a73e1f6a31ca38e7b4ad6ce3b3f32
2123459746 <chia.chang@ge.com>
2016-08-19 00:24:25 -0700

    fixes and updates.

--- model/document.go
+++ model/document.go
@@ -14,7 +14,22 @@
 package model
 
 import (
+	//"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	//"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	//"github.com/gorilla/mux"
+	"github.com/pborman/uuid"
+	"github.build.ge.com/predixsolutions/catalog-onboarding-backend/utils"
+
+	//"log"
+	
+	//"fmt"
+	//"io"
+	//"net/http"
 )
 
 //question type
@@ -25,7 +40,11 @@ const (
 )
 
 type Document struct {
-	DocId        string                    `json:"_did"`
+	Guid        string                    `json:"_id"`
+	Label       string                    `json:"label"`
+	s           *session.Session
+	svc         *s3.S3
+	bucketName  string
 	//Title           string                    `json:"title"`
 	//Desc            string                    `json:"description"`
 	//Type            uint8                     `json:"type"`  //question type
@@ -37,3 +56,36 @@ type Document struct {
 var (
 	//questionnaires map[string]Questionnaire
 )
+
+func InitDoc(accessKeyID, secretAccessKey, bucketName, endpoint string) *Document {
+	
+	region := "us-east-1"
+	
+	disableSSL := true
+	logLevel := aws.LogDebugWithRequestErrors
+	awsConfig := aws.Config{
+		Credentials: credentials.NewStaticCredentials(accessKeyID, secretAccessKey, ""),
+		Region:      &region,
+		Endpoint:    &endpoint,
+		DisableSSL:  &disableSSL,
+		LogLevel:    &logLevel,
+	}
+
+	s := session.New(&awsConfig)
+
+	svc := s3.New(s)
+
+	svc.Handlers.Sign.Clear()
+	svc.Handlers.Sign.PushBack(utils.SignV2)
+
+	return &Document{
+		Guid: uuid.New(),
+		s:          s,
+		svc:        svc,
+		bucketName: bucketName,
+	}
+
+}
+
+
+
